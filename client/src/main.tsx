114b6cee8a1dd0b74bc9445f7f877a055424a496
samy senani <aceportgasd235@gmail.com>
2022-06-24 07:59:40 +0100

    add: custom theme to app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import theme from "./theme";
 
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider resetCSS theme={theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
---
+++ client/src/theme/index.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+import { colors, breakpoints, fonts } from "./utilities";
+
+const theme = extendTheme({
+  colors,
+  breakpoints,
+  fonts,
+  components: {},
+});
+
+export default theme;
